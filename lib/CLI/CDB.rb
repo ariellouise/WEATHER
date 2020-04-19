64406a6d9356476187e48e75657e8c81c23e88e3
ariel murphy <arielladders@gmail.com>
2020-04-19 02:27:18 +0000

    first commit

---
+++ lib/CLI/CDB.rb
@@ -0,0 +1,8 @@
+require "CLI/CDB/version"
+
+module CLI
+  module CDB
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/CLI/CDB/version.rb
@@ -0,0 +1,5 @@
+module CLI
+  module CDB
+    VERSION = "0.1.0"
+  end
+end
